c1c739dd76739cdbac6931107b348e6aad407282
Kenneth E. Jansen <Kenneth.Jansen@colorado.edu>
2016-08-22 22:46:29 -0600

    this version can run flat MPI or OMP with cmake statement -DHAVE_OMP=OFF/ON respectively

--- phSolver/incompressible/hello.f
+++ phSolver/incompressible/hello.f
@@ -1,13 +1,21 @@
       subroutine hello 
       use omp_lib
       include "common.h"
+      integer idshared(4)
 #ifdef HAVE_OMP
 !$OMP  parallel do 
 !$OMP& private (ith,id)
+!$OMP& shared (idshared)
       do ith = 1, 4
           id = omp_get_thread_num ( )
+!$OMP critical
           write (*,*) 'HELLO from rank, process, ith ', myrank,id,ith
+!$OMP end critical
+          idshared(ith)=id
       enddo
+!     do ith = 1, 4
+!         write (*,*) 'HELLO from rank, process, ith ', myrank,idshared(ith),ith
+!     enddo
 #endif
       write (*,*) 'Back to MPI ', myrank
       return
