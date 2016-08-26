cbcb438ae5987849028386db1272fcaabc19b776
Kenneth E. Jansen <Kenneth.Jansen@colorado.edu>
2016-08-25 22:30:48 -0600

    oops  forgot to add a new file eblock.h.....kind of the most important new addition as it is the derived data type to carry private block data

---
+++ phSolver/common/eblock.h
@@ -0,0 +1,11 @@
+      type LocalBlkData
+          integer :: n,s,e,g,l,o,b,t
+!n=nenl, s=nshl, e=nelInThisBlock=npro, g=ngauss, l=lcyst, o=order,b=iblk, t=ith
+      end type LocalBlkData
+
+      type LocalBlkDataB
+          integer :: ni,si,nb,sb,e,g,l,o
+!i=interior, b=boundary
+!n=nenl, s=nshl, e=nelInThisBlock=npro, g=ngauss, l=lcyst, o=order
+      end type LocalBlkDataB
+
