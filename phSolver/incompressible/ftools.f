fca27d4ba04e461c66fb3265f5a7063d6cc1559a
rickybalin <riccardo.balin@colorado.edu>
2019-04-30 17:47:15 -0600

    Fixed problem with intel compiler by reverting to old use of fMtxBlkDot2

--- phSolver/incompressible/ftools.f
+++ phSolver/incompressible/ftools.f
@@ -290,88 +290,17 @@ c Farzin's implementation
 c row and column exchanged
 c--------------------------
 c
-        subroutine fMtxBlkDot2( x, y, c, m, n, rblasphasta,rblasmkl,
-     &             iblasphasta,iblasmkl,ieqswork )
-
-        implicit none
-        include "kmkl.fi"
-
+        subroutine fMtxBlkDot2( x, y, c, m, n )
 c
 c.... Data declaration
 c
+        implicit none
         integer m,      n
-        real*8  x(n,m), y(n),   c(m), d(m)
-
-        real*8 alpha,beta
-        real*8 rdelta, TMRC, rblasphasta,rblasmkl
+        real*8  x(n,m), y(n),   c(m)
 c
         real*8  tmp1,   tmp2,   tmp3,   tmp4
         real*8  tmp5,   tmp6,   tmp7,   tmp8
-        integer i,      j,      m1,lda,incx,incy
-        integer iwork, ieqswork,     icut
-        integer iblasphasta,      iblasmkl
-!DIR$ ASSUME_ALIGNED x: 64, y:64, c:64
-
-      iwork=mod(ieqswork,10)
-
-!       0 chunk8, 1 chunk4, 2  mkl ddot, 3  mkl dgemmv 
-!       4-6  3 for m< icut below, else chunk8, 7 open, 8 double loop 
-        
-        if((iwork.ge.3).and.(iwork.lt.7)) then ! let dgemv do some/all of the work
-          icut=24*2**(iwork-4)
-          if(m.gt.icut) then
-            alpha=1.0
-            beta=0.0
-            incx=1
-            incy=1
-            lda=n
-        
-!   note matrix is expected to be a(m,n) but ours is x(n,m)
-!   I assume we handle this by n<->m in arguments 2, 3
-!   since m is the number of rows of A NOT A^T 
-!  further complicating things A is x a 
-!  their x is our y
-!  their y is our output 
-!  them    y=alpha*A^T x +beta*y
-!  us     d=alpha*x^T y +beta*d
-            rdelta=TMRC()
-            call dgemv('T',n,m,alpha,x,lda,y,incx,beta,c,incy)
-            rblasmkl=rblasmkl+TMRC()-rdelta
-            iblasmkl=iblasmkl+m
-            return
-          else ! jump to chunk 8 to do the rest of the work or 
-              ! change iwork  to who you want to do it
-            iwork=0
-            goto 1
-          endif
-        endif ! end of dgemv
-1       continue 
-        if(iwork.eq.2) then  
-cdir$ ivdep
-          rdelta=TMRC()
-          do i=1,m
-            c(i)=ddot(n,x(1,i),1,y,1)
-          enddo
-          rblasmkl=rblasmkl+TMRC()-rdelta
-          iblasmkl=iblasmkl+m
-          return
-        endif
-        if(iwork.eq.8) then
-          rdelta=TMRC()
-          c=0
-cdir$ ivdep
-          do j = 1, m
-            do i = 1, n
-              c(j) = c(j) + x(i,j) * y(i)
-            enddo
-          enddo
-          rblasphasta=rblasphasta+TMRC()-rdelta
-          iblasphasta=iblasphasta+m
-          return
-        endif
-        if(iwork.eq.0) then
-          rdelta=TMRC()
-
+        integer i,      j,      m1
 c
 c.... Determine the left overs
 c
@@ -533,78 +462,6 @@ c
             c(j+6) = tmp7
             c(j+7) = tmp8
         enddo
-        rblasphasta=rblasphasta+TMRC()-rdelta
-        iblasphasta=iblasphasta+m
-        return
-        endif !iwork=0
-        if(iwork.eq.1) then ! try a max 4 version of original
-          rdelta=TMRC()
-c
-c.... Determine the left overs
-c
-        m1 = mod(m,4) + 1
-
-c
-c.... Do the small pieces
-c
-        goto ( 4001, 1001, 2001, 3001 ) m1
-c
-1001    continue
-        tmp1 = 0
-        do i = 1, n
-            tmp1 = tmp1 + x(i,1) * y(i)
-        enddo
-        c(1) = tmp1
-        goto 4001
-c
-2001    continue
-        tmp1 = 0
-        tmp2 = 0
-        do i = 1, n
-            tmp1 = tmp1 + x(i,1) * y(i)
-            tmp2 = tmp2 + x(i,2) * y(i)
-        enddo
-        c(1) = tmp1
-        c(2) = tmp2
-        goto 4001
-c
-3001    continue
-        tmp1 = 0
-        tmp2 = 0
-        tmp3 = 0
-        do i = 1, n
-            tmp1 = tmp1 + x(i,1) * y(i)
-            tmp2 = tmp2 + x(i,2) * y(i)
-            tmp3 = tmp3 + x(i,3) * y(i)
-        enddo
-        c(1) = tmp1
-        c(2) = tmp2
-        c(3) = tmp3
-        goto 4001
-c
-c.... Do the remaining part
-c
-4001   continue
-c
-        do j = m1, m, 4
-            tmp1 = 0
-            tmp2 = 0
-            tmp3 = 0
-            tmp4 = 0
-            do i = 1, n
-                tmp1 = tmp1 + x(i,j+0) * y(i)
-                tmp2 = tmp2 + x(i,j+1) * y(i)
-                tmp3 = tmp3 + x(i,j+2) * y(i)
-                tmp4 = tmp4 + x(i,j+3) * y(i)
-            enddo
-            c(j+0) = tmp1
-            c(j+1) = tmp2
-            c(j+2) = tmp3
-            c(j+3) = tmp4
-        enddo
-        rblasphasta=rblasphasta+TMRC()-rdelta
-        iblasphasta=iblasphasta+m
-        endif
 c
         return
         end
